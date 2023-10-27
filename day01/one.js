280cd84358b250e6e9401670b58408cfb97dace9
Ben Attenborough <battenborough@gmail.com>
2023-10-28 00:01:37 +0100

    Pipeline approach

--- day01/one.js
+++ day01/one.js
@@ -7,47 +7,15 @@ result.then((data) => {
     console.log(err);
 });
 function main(input) {
-    let block = input.split("\n\n");
-    let line = block.map(x => x.split("\n"));
-    let lineNumber = line.map(x => x.map(y => { return parseInt(y); }));
-    let answerList = lineNumber.map(x => x.reduce((prev, next) => {
-        return prev + next;
-    }, 0));
-    let answer = answerList.reduce((prev, next) => {
-        if (next > prev) {
-            return next;
-        }
-        else {
-            return prev;
-        }
-    }, 0);
-    console.log(`The answer is ${answer}`);
-    let split = input
-        .split("\n\n");
-    let tline = split
+    return input
+        .split("\n\n")
         .map(x => x.split("\n"))
         .map(val => val.map(x => { return parseInt(x); }))
         .map(val => val.reduce((prev, next) => {
         return prev + next;
     }, 0))
         .reduce((prev, next) => {
-        if (next > prev) {
-            return next;
-        }
-        else {
-            return prev;
-        }
+        return (next > prev) ? next : prev;
     }, 0);
-    // .map(line => {
-    //     line
-    //         .split("\n")
-    //     // .map(val => parseInt(val))
-    //     // .reduce((prev, next) => {
-    //     //     return prev + next;
-    //     // }, 0)
-    // })
-    console.log(tline);
-    // return calPerElf.map
-    return 0;
 }
 //# sourceMappingURL=one.js.map
--- day01/one.ts
+++ day01/one.ts
@@ -12,56 +12,14 @@ result.then(
 );
 
 function main(input: string): number {
-    let block: string[] = input.split("\n\n");
-    let line: string[][] = block.map(x => x.split("\n"));
-    let lineNumber: number[][] = line.map(
-        x => x.map(y => { return parseInt(y) })
-    );
-    let answerList: number[] = lineNumber.map(
-        x => x.reduce((prev, next) => {
-            return prev + next;
-        }, 0)
-    )
-    let answer: number = answerList.reduce((prev, next) => {
-        if (next > prev) {
-            return next;
-        } else {
-            return prev;
-        }
-    }, 0)
-
-    console.log(`The answer is ${answer}`);
-
-    let split = input
+    return input
         .split("\n\n")
-
-    let tline: number = split
         .map(x => x.split("\n"))
         .map(val => val.map(x => { return parseInt(x) }))
         .map(val => val.reduce((prev, next) => {
             return prev + next;
         }, 0))
         .reduce((prev, next) => {
-            if (next > prev) {
-                return next;
-            } else {
-                return prev;
-            }
+            return (next > prev) ? next : prev
         }, 0)
-
-    // .map(line => {
-    //     line
-    //         .split("\n")
-    //     // .map(val => parseInt(val))
-    //     // .reduce((prev, next) => {
-    //     //     return prev + next;
-    //     // }, 0)
-    // })
-
-    console.log(tline)
-
-    // return calPerElf.map
-
-
-    return 0;
 }
