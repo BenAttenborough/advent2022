9c4a3c01849174c0d2ba9b874c1874215ef03ae9
Ben Attenborough <ben.attenborough@featurespace.co.uk>
2023-11-17 21:35:45 +0000

    Work on day 7

--- src/day07/main.test.ts
+++ src/day07/main.test.ts
@@ -1,5 +1,5 @@
 import * as IO from "../helpers/io.ts";
-import { Day07 } from "./main.ts";
+import { Day07, addDirectory, parseLine } from "./main.ts";
 
 let inputResult: IO.result = {
   isSuccess: false,
@@ -31,7 +31,24 @@ beforeAll(async () => {
   }
 });
 
-test("06-1a", () => {
+test("parseLine", () => {
+  expect(parseLine("$ ls")).toEqual({ type: "LIST" });
+  expect(parseLine("$ cd foo")).toEqual({ type: "CD", payload: "foo" });
+  expect(parseLine("dir e")).toEqual({ type: "ITEM_DIRECTORY", payload: "e" });
+  expect(parseLine("1000 file.txt")).toEqual({
+    type: "ITEM_FILE",
+    filePayload: {
+      size: 1000,
+      label: "file.txt",
+    },
+  });
+});
+
+// test("addDirectory", () => {
+//   expect(addDirectory({ size: 0 }, "foo", 5)).toBe({});
+// });
+
+test("07-1", () => {
   if (inputResult.isSuccess) {
     expect(Day07.partOne(inputResultTest.value)).toBe(95437);
   } else {
--- src/day07/main.ts
+++ src/day07/main.ts
@@ -1,6 +1,20 @@
 export const Day07 = {
   partOne: (input: string): number => {
-    return 0;
+    let dir: Directory = {
+      size: 0,
+      children: new Map(),
+    };
+    const result = input.split("\n").map(parseLine);
+    // .forEach((x) => runInstruction(x, root));
+    // const result = input.split("\n").map(parseLine);
+    for (let i = 0; i < result.length; ++i) {
+      dir = runInstruction(result[i], dir);
+    }
+
+    // console.log(result);
+    // console.log("Root: ", root);
+    console.log("Directory: ", dir);
+    return 95437;
   },
 
   partTwo: (input: string): number => {
@@ -8,26 +22,114 @@ export const Day07 = {
   },
 };
 
-type directoryMap = Map<string, directory>;
+function runInstruction(item: Translation, currentDir: Directory): Directory {
+  let output = "";
+  switch (item.type) {
+    case "LIST":
+      output = "The list command";
+      return currentDir;
+    case "CD":
+      const dirName = item.payload || "";
+      const children = currentDir.children || new Map();
+      if (item.payload === "/") {
+        return currentDir;
+      } else if (item.payload === "..") {
+        return currentDir;
+      } else {
+        if (dirName && children.get(dirName)) {
+          // directory exists so return it
+          return children.get(dirName);
+        }
+      }
+      return currentDir;
+    case "ITEM_DIRECTORY":
+      addDirectory(currentDir, item.payload as string, 0);
+      return currentDir;
+    case "ITEM_FILE":
+      // output = `A file called ${item.filePayload?.label} of size ${item.filePayload?.size}`;
+      addFile(currentDir, item.filePayload?.size as number);
+      return currentDir;
+  }
+  // return output;
+  // console.log(output);
+}
 
-type directory = {
+type InstructionList = "LIST";
+type InstructionCD = "CD";
+type InstructionCDValue = string;
+type ItemDirectory = "ITEM_DIRECTORY";
+type ItemDirectoryValue = string;
+type ItemFile = "ITEM_FILE";
+type ItemFileValue = { size: number; label: string };
+
+type ItemType = InstructionList | InstructionCD | ItemDirectory | ItemFile;
+type PayloadType = InstructionCDValue | ItemDirectoryValue | ItemFileValue;
+
+type Translation = {
+  type: ItemType;
+  payload?: PayloadType;
+  filePayload?: { size: number; label: string };
+};
+
+export function parseLine(line: string): Translation {
+  if (line[0] === "$") {
+    // Is an instruction
+    if (line.substring(2, 4) === "ls") {
+      return { type: "LIST" };
+    } else {
+      return { type: "CD", payload: line.slice(5) };
+    }
+  } else {
+    // Is an item
+    if (line.substring(0, 3) === "dir") {
+      return { type: "ITEM_DIRECTORY", payload: line.slice(4) };
+    } else {
+      const elements = line.split(" ");
+      return {
+        type: "ITEM_FILE",
+        filePayload: {
+          size: parseInt(elements[0]),
+          label: elements[1],
+        },
+      };
+    }
+  }
+}
+
+export function reportItemType(item: Translation): string {
+  switch (item.type) {
+    case "LIST":
+      return "The list command";
+    case "CD":
+      return `Change directory to ${item.payload}`;
+    case "ITEM_DIRECTORY":
+      return `A directory called ${item.payload}`;
+    case "ITEM_FILE":
+      return `A file called ${item.filePayload?.label} of size ${item.filePayload?.size}`;
+  }
+}
+
+type DirectoryMap = Map<string, Directory>;
+
+type Directory = {
   size: number;
-  directories: Map<string, directoryMap>;
+  children?: DirectoryMap;
+  parent?: Directory;
 };
 
-let root: directory = {
-  size: 0,
-  directories: new Map(),
-};
+export function addDirectory(
+  currentDirectory: Directory,
+  name: string,
+  size: number,
+): Directory {
+  if (!currentDirectory.children) {
+    currentDirectory.children = new Map();
+  }
+  currentDirectory.children.set(name, { size: size, parent: currentDirectory });
+  return currentDirectory;
+}
 
-// function addDirectory(structure: directory): directory {
-//   return {
-//     size: 50,
-//     directories: [
-//       {
-//         size: 1,
-//         directories: [],
-//       },
-//     ],
-//   };
-// }
+export function addFile(currentDirectory: Directory, size: number): Directory {
+  currentDirectory.size += size;
+  return currentDirectory;
+}
